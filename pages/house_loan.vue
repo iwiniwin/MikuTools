2a961658d01b2922048895e864371d46af718ea8
iwiniwin <iwiniwin@foxmail.com>
2022-01-20 00:44:27 +0800

    调整房贷计算器样式

--- pages/house_loan.vue
+++ pages/house_loan.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="house_loan">
-        <nya-container title="屏幕录制">
+        <nya-container title="房贷计算器">
             <nya-input v-model.trim="loanAmount" fullwidth type="number" label="贷款金额（万元）" autofocus placeholder="258" autocomplete="off" />
             <nya-input v-model.trim="loanTerm" fullwidth type="number" label="贷款期限（年）" autofocus placeholder="30" autocomplete="off" />
             <nya-input v-model.trim="loanRate" fullwidth type="number" label="贷款利率（%）" autofocus placeholder="5.45" autocomplete="off" />
-            <nya-checkbox v-model="choosePrepayment" class="mt-15" label="提前还款" />
+            <nya-checkbox v-model="choosePrepayment" class="mb-15" label="提前还款" />
+            <br>
             <div v-if="choosePrepayment">
                 <client-only>
-                    <date-picker v-model="firstRepaymentDate" class="nya-input" :editable="false" placeholder="第一次还款时间" :default-value="firstRepaymentDate" value-type="format" :not-before="from" :not-after="to" />
-                    <date-picker v-model="intendedRepaymentDate" class="nya-input" :editable="false" placeholder="预计提前还款时间" :default-value="intendedRepaymentDate" value-type="format" :not-before="from" :not-after="to" />
+                    <label class="input-title">第一次还款时间</label>
+                    <date-picker v-model="firstRepaymentDate" class="nya-input" :editable="false" placeholder="第一次还款时间" :default-value="firstRepaymentDateDefaultValue" value-type="format" :not-before="from" :not-after="to" />
+                    <label class="input-title">预计提前还款时间</label>
+                    <date-picker v-model="intendedRepaymentDate" class="nya-input" :editable="false" placeholder="预计提前还款时间" :default-value="intendedRepaymentDateDefaultValue" value-type="format" :not-before="from" :not-after="to" />
                     <nya-input v-model.trim="intendedRepaymentAmount" fullwidth type="number" label="预计提前还款金额（万元）" autofocus placeholder="30" autocomplete="off" />
                 </client-only>
             </div>
@@ -33,10 +36,12 @@ if (process.browser) {
     DatePicker = require('vue2-datepicker').default;
 }
 import dayjs from 'dayjs';
+import Dynamic from '@/components/Dynamic';
 export default {
     name: 'HouseLoan',
     components: {
-        DatePicker
+        DatePicker,
+        Dynamic
     },
     head() {
         return this.$store.state.currentTool.head;
@@ -50,7 +55,10 @@ export default {
             firstRepaymentDate: '',
             intendedRepaymentDate: '',
             intendedRepaymentAmount: 30,
-            defaultValue: dayjs()
+            firstRepaymentDateDefaultValue: dayjs()
+                .subtract(18, 'year')
+                .format('YYYY-MM-DD'),
+            intendedRepaymentDateDefaultValue: dayjs()
                 .subtract(18, 'year')
                 .format('YYYY-MM-DD'),
             from: dayjs()
@@ -77,11 +85,18 @@ export default {
     .nya-input {
         width: 100%;
         box-sizing: border-box;
+        margin-bottom: 15px;
         .mx-input {
             border-radius: 0;
             height: 100%;
             box-sizing: border-box;
         }
+    }
+    .input-title {
+        display: block;
+        font-size: 16px;
+        margin-bottom: 10px;
+        font-weight: bold;
     }
 }
 </style>
