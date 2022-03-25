3067634b5757357528fe7c01e03fc2082c2876cf
iwiniwin <iwiniwin@foxmail.com>
2022-03-25 18:40:38 +0800

    修改现金卡数据读取方式

--- pages/cash_card_format.vue
+++ pages/cash_card_format.vue
@@ -1,59 +1,70 @@
 <template>
     <div class="cash_card_format">
         <nya-container title="现金卡格式转换">
-            <nya-input
-                v-model="n"
-                fullwidth
-                class="mb-15"
-                type="file"
-                accept=".xls,.xlsx"
-                label="请选择要转换的现金卡文档"
-                placeholder="点击这里上传文件"
-                @change="readExcel($event)"
-            />
+            <nya-input v-model="content" fullwidth rows="10" type="textarea" autofocus autocomplete="off" label="待转换的内容" placeholder="请输入要处理的内容" />
+            <button type="button" class="nya-btn" @click="conversion">开始转换</button>
         </nya-container>
         <nya-container v-show="result" title="转换成功">
+            <button type="button" class="nya-btn" @click="copy">复制</button>
             <pre>{{ result }}</pre>
         </nya-container>
     </div>
 </template>
 <script>
-import XLSX from "xlsx";
 
 export default {
     data: () => ({
-        n: '',
+        content: '',
         result: '' //输出的结果
     }),
     methods: {
-        readExcel(e) {
-            // 读取表格文件
-            const files = e.target.files;
-            if (files.length <= 0) {
-                return false;
-            } else if (!/\.(xls|xlsx)$/.test(files[0].name.toLowerCase())) {
-                this.$message({
-                    message: "上传格式不正确，请上传xls或者xlsx格式",
-                    type: "warning"
-                });
-                return false;
+        conversion() {
+            var data = this.content.split("\n");
+            if(data[data.length -1] == "") {
+                data.pop();
             }
-
-            const fileReader = new FileReader();
-            fileReader.onload = ev => {
-                try {
-                    const data = ev.target.result;
-                    const workbook = XLSX.read(data, {
-                        type: "binary"
-                    });
-                    const wsname = workbook.SheetNames[0]; //取第一张表
-                    const ws = XLSX.utils.sheet_to_json(workbook.Sheets[wsname]); //生成json表格内容
-                    console.log(ws);
-                } catch (e) {
-                    return false;
+            var copyContent = "";
+            for (let index = 0; index < data.length; index++) {
+                var pair = data[index].split("\t");
+                if(pair[0].length == 18 && pair[1].length == 14) {
+                    copyContent += "        {";
+                    copyContent += ("\n            \"card\":\"" + pair[0] + "\",");
+                    copyContent += ("\n            \"pwd\":\"" + pair[1] + "\"");
+                    if(index == data.length - 1)
+                    {
+                        copyContent += "\n        }";
+                    }
+                    else
+                    {
+                        copyContent += "\n        },\n";
+                    }
                 }
-            };
-            fileReader.readAsBinaryString(files[0]);
+                else if(index != 0) {
+                    console.log("数据v异常" + data[index]);
+                    return;
+                }
+            }
+            this.result = copyContent;
+        },
+        copy() {
+            this.copyText(this.result);
+        },
+        copyText(value){
+            const textarea = document.createElement('textarea');
+            // 将该 textarea 设为 readonly 防止 iOS 下自动唤起键盘，同时将 textarea 移出可视区域
+            textarea.readOnly = 'readonly';
+            textarea.style.position = 'absolute';
+            textarea.style.left = '-9999px';
+            // 将要 copy 的值赋给 textarea 标签的 value 属性  
+            // 网上有些例子是赋值给innerText,这样也会赋值成功，但是识别不了\r\n的换行符，赋值给value属性就可以
+            textarea.value = value;
+            // 将 textarea 插入到 body 中
+            document.body.appendChild(textarea);
+            // 选中值并复制
+            textarea.select();
+            textarea.setSelectionRange(0, textarea.value.length);
+            document.execCommand('Copy');
+            document.body.removeChild(textarea);
         }
     }
 };
